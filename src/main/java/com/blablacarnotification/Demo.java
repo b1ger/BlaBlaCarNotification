aeefb333db491d51c7a634447d819ff6f9ddf0f5
Bogdan Herashchenko <bohdan.herashchenko@volia.com>
2018-03-01 11:37:59 +0200

    Small refactoring

--- src/main/java/com/blablacarnotification/Demo.java
+++ src/main/java/com/blablacarnotification/Demo.java
@@ -2,8 +2,9 @@ package com.blablacarnotification;
 
 public class Demo {
     public static void main(String[] args) {
+        String url = "https://www.blablacar.com.ua/ride-sharing/kijiv/kaniv/?fn=Kyiv%2C+%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0&fc=50.4501%7C30.5234&fcc=UA&fp=0&tn=Kaniv%2C+%D0%A7%D0%B5%D1%80%D0%BA%D0%B0%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C+%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0&tc=49.751033%7C31.4697&tcc=UA&tp=0&db=02%2F03%2F2018&sort=trip_date&order=asc&limit=10&page=1&v=default&s_uuid=7fd691da-9261-4511-8c88-dfdfb1bafc3d";
         Parser parser = new Parser();
 
-        System.out.println(parser.process("https://www.blablacar.com.ua/ride-sharing/kijiv/kaniv/?fn=Kyiv%2C+%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0&fc=50.4501%7C30.5234&fcc=UA&fp=0&tn=Kaniv%2C+%D0%A7%D0%B5%D1%80%D0%BA%D0%B0%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C+%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0&tc=49.751033%7C31.4697&tcc=UA&tp=0&db=02%2F03%2F2018&sort=trip_date&order=asc&limit=10&page=1&v=default&s_uuid=7fd691da-9261-4511-8c88-dfdfb1bafc3d"));
+        parser.createDom(parser.getHtml(url));
     }
 }
--- src/main/java/com/blablacarnotification/Parser.java
+++ src/main/java/com/blablacarnotification/Parser.java
@@ -1,5 +1,16 @@
 package com.blablacarnotification;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +19,7 @@ import java.net.URL;
 
 public class Parser {
 
-    public String process(String urlString) {
+    public String getHtml(String urlString) {
         StringBuilder stringBuilder = new StringBuilder();
 
         try {
@@ -38,5 +49,23 @@ public class Parser {
         }
 
         return stringBuilder.toString();
+    }
+
+    public void createDom(String uri) {
+//        NodeList nodeList = null;
+//        try {
+//            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+//            Document doc = builder.parse(uri);
+//
+//            XPath xPath = XPathFactory.newInstance().newXPath();
+//            nodeList = (NodeList) xPath.evaluate("body", doc.getDocumentElement(), XPathConstants.NODESET);
+//        } catch (IOException |
+//                SAXException |
+//                ParserConfigurationException |
+//                XPathExpressionException ex) {
+//            System.err.println(ex.getMessage());
+//        }
+
+        //TODO
     }
 }
